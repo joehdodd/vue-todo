9629ef03823110aa30ae844ebb411d08d51e5252
Joe <joehdodd@gmail.com>
2018-03-22 09:45:21 -0400

    addTodo method

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
+    <form @submit.prevent="addTodo">
+      <input v-model="text">
+      <button>Add Todo</button>
+    </form>
     <ToDoList :todos="todos"/>
   </div>
 </template>
 
 <script>
-import ToDoList from './components/ToDoList.vue'
+import ToDoList from './components/ToDoList.vue';
 
 export default {
   name: 'app',
   data() {
     return {
       todos: [
-        { task: 'Take out the trash'},
-        { task: 'Be cool'},
-        { task: 'Ride the train'},
-        { task: 'Life'}
-      ]
+        { task: 'Take out the trash' },
+        { task: 'Be cool' },
+        { task: 'Ride the train' },
+        { task: 'Life' }
+      ],
+      text: ''
+    };
+  },
+  methods: {
+    addTodo() {
+      const { text, todos } = this;
+      return text.length ? todos.push({ task: text }) : alert('Please enter some text.')
     }
   },
   components: {
     ToDoList
   }
-}
+};
 </script>
 
 <style>
@@ -33,5 +44,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+form,
+input,
+button {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  color: #2c3e50;
+}
+
+button {
+  border: none;
+  border-radius: 3px;
+  height: 30px;
+  width: 90px;
 }
 </style>
